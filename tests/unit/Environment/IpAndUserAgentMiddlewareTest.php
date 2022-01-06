6d2a8ff258c1cbc2ccf37a69d56d6b96a2066317
Oleg Kasyanov <oleg@batumi.org>
2022-01-06 11:33:46 +0400

    Improved IpAndUserAgentMiddlewareTest

--- tests/unit/Environment/IpAndUserAgentMiddlewareTest.php
+++ tests/unit/Environment/IpAndUserAgentMiddlewareTest.php
@@ -5,219 +5,231 @@ declare(strict_types=1);
 namespace MobicmsTest\System\Environment;
 
 use Mobicms\System\Environment\IpAndUserAgentMiddleware;
-use Mockery;
-use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 /**
  * @psalm-suppress MissingConstructor
+ * @psalm-suppress MixedMethodCall
+ * @psalm-suppress PossiblyInvalidArgument
+ * @psalm-suppress PossiblyUndefinedMethod
  */
-class IpAndUserAgentMiddlewareTest extends MockeryTestCase
+class IpAndUserAgentMiddlewareTest extends TestCase
 {
+    private ServerRequestInterface|MockObject $request;
+
+    public function setUp(): void
+    {
+        $this->request = $this->createMock(ServerRequestInterface::class);
+    }
+
     public function testDetermineIpAddress(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('getServerParams')
-            ->once()
-            ->andReturn(['REMOTE_ADDR' => '31.23.209.1']);
+        $this->request
+            ->expects($this->once())
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '31.23.209.1']);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('31.23.209.1', $middleware->determineIpAddress($request));
+        $this->assertSame('31.23.209.1', $middleware->determineIpAddress($this->request));
     }
 
     public function testDetermineIpAddressWithInvalidIp(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('getServerParams')
-            ->once()
-            ->andReturn(['REMOTE_ADDR' => '392.268.0.9']);
+        $this->request
+            ->expects($this->once())
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '392.268.0.9']);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertNull($middleware->determineIpAddress($request));
+        $this->assertNull($middleware->determineIpAddress($this->request));
     }
 
     public function testDetermineIpAddressWithoutIp(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('getServerParams')
-            ->once()
-            ->andReturn([]);
+        $this->request
+            ->expects($this->once())
+            ->method('getServerParams')
+            ->willReturn([]);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertNull($middleware->determineIpAddress($request));
+        $this->assertNull($middleware->determineIpAddress($this->request));
     }
 
     public function testDetermineIpViaProxyAddress(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->expects($this->once())
+            ->method('hasHeader')
             ->with('Forwarded')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->expects($this->once())
+            ->method('getHeaderLine')
             ->with('Forwarded')
-            ->once()
-            ->andReturn('212.58.119.76, 91.221.6.36');
-        $request->shouldReceive('getServerParams')
-            ->andReturn(['REMOTE_ADDR' => '31.23.209.1']);
+            ->willReturn('212.58.119.76, 91.221.6.36');
+        $this->request
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '31.23.209.1']);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($request));
+        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($this->request));
     }
 
     public function testDetermineIpViaProxyAddressSkipPrivateNetworks(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->expects($this->once())
+            ->method('hasHeader')
             ->with('Forwarded')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->expects($this->once())
+            ->method('getHeaderLine')
             ->with('Forwarded')
-            ->once()
-            ->andReturn('10.0.0.1, 172.16.0.1, 192.168.0.1, 212.58.119.76');
-        $request->shouldReceive('getServerParams')
-            ->andReturn(['REMOTE_ADDR' => '31.23.209.1']);
+            ->willReturn('10.0.0.1, 172.16.0.1, 192.168.0.1, 212.58.119.76');
+        $this->request
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '31.23.209.1']);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($request));
+        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($this->request));
     }
 
     public function testDetermineIpViaProxyAddressSkipSameIpAsRemote(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->expects($this->once())
+            ->method('hasHeader')
             ->with('Forwarded')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->expects($this->once())
+            ->method('getHeaderLine')
             ->with('Forwarded')
-            ->once()
-            ->andReturn('31.23.209.1, 212.58.119.76');
-        $request->shouldReceive('getServerParams')
-            ->andReturn(['REMOTE_ADDR' => '31.23.209.1']);
+            ->willReturn('31.23.209.1, 212.58.119.76');
+        $this->request
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '31.23.209.1']);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($request));
+        $this->assertSame('212.58.119.76', $middleware->determineIpViaProxyAddress($this->request));
     }
 
     public function testDetermineIpViaProxyAddressWithoutValidIp(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
-            ->andReturn('331.23.209.1, test');
+        $this->request
+            ->method('hasHeader')
+            ->willReturn(true);
+        $this->request
+            ->method('getHeaderLine')
+            ->willReturn('331.23.209.1, test');
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertNull($middleware->determineIpViaProxyAddress($request));
+        $this->assertNull($middleware->determineIpViaProxyAddress($this->request));
     }
 
     public function testDetermineIpViaProxyAddressWithoutRequiredHeaders(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
-            ->andReturn(false);
+        $this->request
+            ->method('hasHeader')
+            ->willReturn(false);
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertNull($middleware->determineIpViaProxyAddress($request));
+        $this->assertNull($middleware->determineIpViaProxyAddress($this->request));
     }
 
     public function testDetermineUserAgent(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->expects($this->once())
+            ->method('hasHeader')
             ->with('User-Agent')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->expects($this->once())
+            ->method('getHeaderLine')
             ->with('User-Agent')
-            ->once()
-            ->andReturn('Test User Agent');
+            ->willReturn('Test User Agent');
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('Test User Agent', $middleware->determineUserAgent($request));
+        $this->assertSame('Test User Agent', $middleware->determineUserAgent($this->request));
     }
 
     public function testDetermineUserAgentTrimLongStringTo255Symbols(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->method('hasHeader')
             ->with('User-Agent')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->method('getHeaderLine')
             ->with('User-Agent')
-            ->once()
-            ->andReturn(str_repeat('a', 300));
+            ->willReturn(str_repeat('a', 300));
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame(str_repeat('a', 255), $middleware->determineUserAgent($request));
+        $this->assertSame(str_repeat('a', 255), $middleware->determineUserAgent($this->request));
     }
 
     public function testDetermineUserAgentSanitizeSpecialChars(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->method('hasHeader')
             ->with('User-Agent')
-            ->once()
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
+            ->willReturn(true);
+        $this->request
+            ->method('getHeaderLine')
             ->with('User-Agent')
-            ->once()
-            ->andReturn('&"\'<>');
+            ->willReturn('&"\'<>');
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertSame('&amp;&quot;&#039;&lt;&gt;', $middleware->determineUserAgent($request));
+        $this->assertSame('&amp;&quot;&#039;&lt;&gt;', $middleware->determineUserAgent($this->request));
     }
 
     public function testDetermineUserAgentWithoutRequiredHeaders(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('hasHeader')
+        $this->request
+            ->method('hasHeader')
             ->with('User-Agent')
-            ->once()
-            ->andReturn(false);
-        $request->shouldNotReceive('getHeaderLine');
+            ->willReturn(false);
+        $this->request
+            ->expects($this->never())
+            ->method('getHeaderLine');
 
         $middleware = new IpAndUserAgentMiddleware();
-        $this->assertNull($middleware->determineUserAgent($request));
+        $this->assertNull($middleware->determineUserAgent($this->request));
     }
 
     public function testProcess(): void
     {
-        $request = Mockery::mock(ServerRequestInterface::class);
-
         // Check determime IP
-        $request->shouldReceive('getServerParams')
-            ->andReturn(['REMOTE_ADDR' => '192.168.0.9']);
-        $request->shouldReceive('withAttribute')
-            ->with(IpAndUserAgentMiddleware::IP_ADDR, '192.168.0.9')
-            ->once()
-            ->andReturn($request);
-
-        // Check determine IP via Proxy
-        $request->shouldReceive('hasHeader')
-            ->with('Forwarded')
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
-            ->with('Forwarded')
-            ->andReturn('212.58.119.76, 91.221.6.36');
-        $request->shouldReceive('withAttribute')
-            ->with(IpAndUserAgentMiddleware::IP_VIA_PROXY_ADDR, '212.58.119.76')
-            ->once()
-            ->andReturn($request);
-
-        // Check determine User Agent
-        $request->shouldReceive('hasHeader')
-            ->with('User-Agent')
-            ->andReturn(true);
-        $request->shouldReceive('getHeaderLine')
-            ->with('User-Agent')
-            ->andReturn('Test User Agent');
-        $request->shouldReceive('withAttribute')
-            ->with(IpAndUserAgentMiddleware::USER_AGENT, 'Test User Agent')
-            ->once()
-            ->andReturn($request);
-
-        $handler = Mockery::mock(RequestHandlerInterface::class);
-        $handler->shouldReceive('handle')
-            ->with($request)
-            ->once()
-            ->andReturn(Mockery::mock(ResponseInterface::class));
-
+        $this->request
+            ->method('getServerParams')
+            ->willReturn(['REMOTE_ADDR' => '192.168.0.9']);
+        $this->request
+            ->method('withAttribute')
+            ->withConsecutive(
+                [IpAndUserAgentMiddleware::IP_ADDR, '192.168.0.9'],
+                [IpAndUserAgentMiddleware::IP_VIA_PROXY_ADDR, '212.58.119.76'],
+                [IpAndUserAgentMiddleware::USER_AGENT, 'Test User Agent']
+            )
+            ->willReturn($this->request);
+        $this->request
+            ->method('hasHeader')
+            ->withConsecutive(
+                ['Forwarded'],
+                ['User-Agent']
+            )
+            ->willReturn(true);
+        $this->request
+            ->method('getHeaderLine')
+            ->withConsecutive(
+                ['Forwarded'],
+                ['User-Agent']
+            )
+            ->willReturn(
+                '212.58.119.76, 91.221.6.36',
+                'Test User Agent'
+            );
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler
+            ->expects($this->once())
+            ->method('handle')
+            ->with($this->request)
+            ->willReturn($this->createMock(ResponseInterface::class));
         $middleware = new IpAndUserAgentMiddleware();
-        $middleware->process($request, $handler);
+        $middleware->process($this->request, $handler);
     }
 }
